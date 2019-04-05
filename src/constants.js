0e4ddd2b846a02aae19208a3a86428d81724a58b
thedocbwarren <jedi.master@augmentedjs.com>
2019-04-04 18:22:46 -0700

    first start

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const FONT = {
+  FONTS: "https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto:100,300,400,600,400italic",
+  ICONS: "https://fonts.googleapis.com/icon?family=Material+Icons",
+  BUTTONS: "https://fonts.googleapis.com/css?family=Libre+Franklin"
+};
+
+export const APP_NAME = "Facet Examples";
+export const TITLE = APP_NAME;
