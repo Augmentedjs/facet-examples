87b1ac84dfb93ddedaa4b3a0440d69ee8a1f8cea
thedocbwarren <jedi.master@augmentedjs.com>
2019-04-05 23:35:09 -0700

    using new version and trying to style

--- src/views/facetView.js
+++ src/views/facetView.js
@@ -4,13 +4,6 @@ import Logger from "../logger/logger.js";
 const MOUNT_POINT = "#facet";
 const NAME = "filterview";
 
-/*
-<h2 data-${view.name}="${FIELDS.PROJECT.id}" data-click="collapse" class="toggle collapse">${FIELDS.PROJECT.name}</h2>
-<div id="${FIELDS.PROJECT.id}" class="toggle collapse">
-  ${projectCheck}
-</div>
-*/
-
 class FacetView extends BaseFacetView {
   constructor(options) {
     super({
