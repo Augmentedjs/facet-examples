5ea96f0bff2fd2422d5c76c71f620e5b35f3f0a4
thedocbwarren <jedi.master@augmentedjs.com>
2019-04-07 19:56:02 -0700

    adding correct styles, updated component version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import material from "presentation-css";
+import fsass from "../node_modules/presentation-facets/src/styles/main.scss";
 import sass from "./styles/main.scss";
 
 import Logger from "./logger/logger.js";
