29c33e64c60e5784e4a796fb7f52593ba29290f2
thedocbwarren <jedi.master@augmentedjs.com>
2019-04-07 23:00:39 -0700

    simple example

--- src/views/facetView.js
+++ src/views/facetView.js
@@ -3,31 +3,58 @@ import BaseFacetView from "presentation-facets";
 import Logger from "../logger/logger.js";
 const MOUNT_POINT = "#facet";
 const NAME = "filterview";
+const FILTERS = [
+  { "id": "characters",
+    "name": "Characters",
+    "collection": [
+      { "name": "Xzar", "value": "xzar" },
+      { "name": "Montaron", "value": "montaron" },
+      { "name": "Noober", "value": "noober" }
+    ]
+  },
+  {
+    "id": "locations",
+    "name": "Locations",
+    "collection": [
+      { "name": "Candlekeep", "value": "candlekeep" },
+      { "name": "Cloakwood", "value": "cloakwood" },
+      { "name": "Nashkel", "value": "nashkel" }
+    ]
+  },
+  {
+    "id": "creatures",
+    "name": "Creatures",
+    "collection": [
+      { "name": "Carrion Crawler", "value": "carrioncrawler" },
+      { "name": "Ankheg", "value": "ankheg" },
+      { "name": "Ogre", "value": "ogre" },
+      { "name": "Ettercap", "value": "ettercap" },
+      { "name": "Gnoll", "value": "gnoll" },
+      { "name": "Skeleton", "value": "skeleton" },
+      { "name": "Ogre Mage", "value": "ogremage" },
+      { "name": "Black Bear", "value": "blackbear" },
+      { "name": "Polar bear", "value": "polarbear" },
+      { "name": "Kobold", "value": "kobold" },
+      { "name": "Wolf", "value": "wolf" }
+    ]
+  }
+];
 
 class FacetView extends BaseFacetView {
   constructor(options) {
     super({
       "el": MOUNT_POINT,
       "name": NAME,
-      "style": "view examples"
+      "style": "view",
+      "title": "Balder's Gate",
+      "filters": FILTERS,
+      "button": "Filter"
     });
-
-    const id = "id",
-          name = "name",
-          collection = [
-            { "name": "Bubba", "value": "sux" },
-            { "name": "Noober", "value": "heya" }
-          ];
-    this.addFilter(id, name, collection);
   };
-
-  render() {
-    Logger.debug("FacetView render()");
-    return super.render();
-  };
-
-  remove() {
-    return super.remove();
+  submit(e) {
+    e.preventDefault();
+    Logger.info(JSON.stringify(this.selections));
+    return this;
   };
 };
 
